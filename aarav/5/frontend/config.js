06e53da1e238520d5de064cb47c8eac343a381af
Shritiz <shritizsingh428@gmail.com>
2026-09-13 19:00:26 +0530

    love

---
+++ aarav/5/frontend/config.js
@@ -0,0 +1,2 @@
+// Set this to your deployed API URL. Leave empty when serving frontend through the local backend.
+window.APP_CONFIG = { API_BASE_URL: 'https://shritiz.github.io/Will-u-/aarav/5' };
